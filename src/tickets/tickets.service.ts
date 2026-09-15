8b8200767b50c3ff9009b03cf7a5abd4d7060ac6
saikot05 <saikotislam08@gmail.com>
2026-09-15 23:13:29 +0600

    feat(tickets): update create method to use CreateTicketDto for ticket creation

--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Ticket } from './ticket.interface.js';
+import { CreateTicketDto } from './dto/create-ticket.dto.js';
 
 @Injectable()
 export class TicketsService {
@@ -51,12 +52,12 @@ export class TicketsService {
         return ticket;
     }
 
-    create(payload: any){
+    create(createTicketDto: CreateTicketDto){
         const ticket: Ticket = {
             id: this.nextTicketId++,
-            subject: payload.subject,
-            description: payload.description,
-            priority: payload.priority,
+            subject: createTicketDto.subject,
+            description: createTicketDto.description,
+            priority: createTicketDto.priority,
             status: 'open',
             createdAt: new Date().toISOString(),
         };
