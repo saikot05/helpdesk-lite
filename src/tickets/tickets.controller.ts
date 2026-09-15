92d8fe5a9b94e1620ad8334e1fcddf9b37a93fab
saikot05 <saikotislam08@gmail.com>
2026-09-15 10:49:15 +0600

    refactor: remove unused AppController and AppService; implement Tickets module with controller and service

---
+++ src/tickets/tickets.controller.ts
@@ -0,0 +1,11 @@
+import { Controller,Get } from '@nestjs/common';
+import { TicketsService } from './tickets.service.js';
+
+@Controller('tickets')
+export class TicketsController {
+    constructor(private readonly ticketsService:TicketsService){}
+    @Get()
+    findAll(){
+        return this.ticketsService.findAll();
+    }
+}
